064c15ba5c918b14b5599566aecdff3988e5953e
Alina Ilea <mailea@cisco.com>
2021-04-28 15:31:41 +0300

    feat(Storybook): test2

--- src/components/hooks/useActivity.js
+++ src/components/hooks/useActivity.js
@@ -28,6 +28,7 @@ export default function useActivity(activityID) {
 
     return () => {
       subscription.unsubscribe();
+      console.log('gggg');
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
